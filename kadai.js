365874ffbb5a629ae0da9154745dbb2824fe2f9a
zm17494 <zm17494@josai.ac.jp>
2019-02-03 07:56:16 -0500

    message

--- kadai.js
+++ kadai.js
@@ -1,19 +1,39 @@
 function showimg1(){
-document.area1.src = "1nenn.jpg";
+document.area1.src = "./image/ピッチャー.jpg";
 }
 
 function showimg2(){
-document.area1.src = "2nenn.jpg";
+document.area1.src = "./image/キャッチャー.jpg";
 }
 
 function showimg3(){
-document.area1.src = "3nenn.jpg";
+document.area1.src = "./image/ファースト.jpg";
 }
 
 function showimg4(){
-document.area1.src = "4nenn.jpg";
+document.area1.src = "./image/セカンド.jpg";
 }
             
 function showimg5(){
-document.area1.src = "zenntai.jpg";
+document.area1.src = "./image/ショート.jpg";
+}
+
+function showimg6(){
+document.area1.src = "./image/サード.jpg";
+}
+
+function showimg7(){
+document.area1.src = "./image/レフト.jpg";
+}
+
+function showimg8(){
+document.area1.src = "./image/センター.jpg";
+}
+
+function showimg9(){
+document.area1.src = "./image/ライト.jpg";
+}
+
+function showimg10(){
+document.area1.src = "./image/マネージャー.jpg";
 }
